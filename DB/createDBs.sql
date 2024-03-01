cd25609744958c3b293644de2c6f4b1a5941505e
sakJH <79791046+sakJH@users.noreply.github.com>
2024-03-01 14:24:46 +0100

    Update createDBs.sql

--- DB/createDBs.sql
+++ DB/createDBs.sql
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS Memberships (
     MembershipID INT AUTO_INCREMENT PRIMARY KEY,
     UserID INT NOT NULL,
     MembershipType VARCHAR(255) NOT NULL,
+    MenbershipPrice DECIMAL(10, 2) NOT NULL,
     StartDate DATE NOT NULL,
     EndDate DATE NOT NULL,
     FOREIGN KEY (UserID) REFERENCES GymNestUserDB.Users(UserID) ON DELETE CASCADE
