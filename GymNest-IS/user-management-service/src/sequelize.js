9a7da4a7ddf8a5e06d9ea3ba34c7bdf09b8c9291
sakacj <sakacja1@uhk.cz>
2024-03-22 16:31:43 +0100

    snaha o opravu db

--- GymNest-IS/user-management-service/src/sequelize.js
+++ GymNest-IS/user-management-service/src/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // Načtení proměnných z .env souboru
 
-const sequelize = new Sequelize( //TODO - nastavení DB login a heslo v .env mimo mikroslužbu
+const sequelize = new Sequelize( // Nastavení DB login a heslo v .env
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
