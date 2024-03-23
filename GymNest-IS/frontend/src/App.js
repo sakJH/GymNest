0ba6bd7d1a77c108e644b11e6fbea021b8b21cc1
bourama1 <bourama1@uhk.cz>
2024-03-23 17:39:39 +0100

    frontend - prihlaseni/registrace

--- GymNest-IS/frontend/src/App.js
+++ GymNest-IS/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -8,19 +9,23 @@ import ActionsPage from './pages/ActionsPage';
 import MembershipPage from './pages/MembershipPage';
 import SchledulePage from './pages/SchledulePage';
 
+const clientId = "VÁŠ_CLIENT_ID_Z_GCP"; // TODO nebo jiz je v backendu?
+
 function App() {
   return (
-    <Router>
-      <CssBaseline /> {/* Resetuje CSS pro konzistentní vzhled */} 
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} /> {/* Přidáno */}
-        <Route path="/akce" element={<ActionsPage />} />
-        <Route path="/clenstvi" element={<MembershipPage />} />
-        <Route path="/rozvrhy" element={<SchledulePage />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <GoogleOAuthProvider clientId={clientId}>
+      <Router>
+        <CssBaseline /> {/* Resetuje CSS pro konzistentní vzhled */} 
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} /> {/* Přidáno */}
+          <Route path="/akce" element={<ActionsPage />} />
+          <Route path="/clenstvi" element={<MembershipPage />} />
+          <Route path="/rozvrhy" element={<SchledulePage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
--- GymNest-IS/frontend/src/pages/HomePage.js
+++ GymNest-IS/frontend/src/pages/HomePage.js
@@ -1,15 +1,63 @@
-import React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Typography, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Typography, Box, Alert } from '@mui/material';
 
 function HomePage() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  // Stavové proměnné pro uchování uživatelského vstupu
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
-  const handleClickOpen = () => {
+  const handleLoginSuccess = (credentialResponse) => {
+    console.log('Přihlášení/Registrace úspěšná! Credential Response:', credentialResponse);
+    handleClose(); // Zavře dialog po úspěšné autentizaci
+  };
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: handleLoginSuccess,
+    onError: error => console.log(error),
+  });
+
+  const handleClickOpen = (login) => {
+    setIsLogin(login);
     setOpen(true);
+    // Reset stavových proměnných formuláře
+    setEmail('');
+    setPassword('');
+    setFirstName('');
+    setLastName('');
+    setPasswordError('');
   };
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const checkPasswordStrength = (password) => {
+    // Jednoduchá kontrola síly hesla (můžete přidat další podmínky)
+    if(password.length < 8) {
+      return "Heslo musí být alespoň 8 znaků dlouhé.";
+    }
+    if(!/\d/.test(password) || !/[a-zA-Z]/.test(password)) {
+      return "Heslo musí obsahovat písmena a čísla.";
+    }
+    return "";
+  };
+
+  const handleSubmit = () => {
+    if (!isLogin) { // Kontrola probíhá pouze při registraci
+      const passwordStrengthError = checkPasswordStrength(password);
+      if(passwordStrengthError) {
+        setPasswordError(passwordStrengthError);
+        return; // Přeruší odeslání formuláře
+      }
+    }
+    console.log('Formulář odeslán', { email, password, firstName, lastName });
+    handleClose(); // Zavře dialog po odeslání formuláře
   };
 
   return (
@@ -17,11 +65,14 @@ function HomePage() {
       <Typography variant="h4" gutterBottom>
         Vítejte na GymNest
       </Typography>
-      <Button variant="outlined" onClick={handleClickOpen}>
+      <Button variant="outlined" onClick={() => handleClickOpen(true)} sx={{ marginRight: 1 }}>
         Přihlásit se
       </Button>
+      <Button variant="outlined" onClick={() => handleClickOpen(false)}>
+        Registrovat se
+      </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Přihlášení</DialogTitle>
+        <DialogTitle>{isLogin ? "Přihlášení" : "Registrace"}</DialogTitle>
         <DialogContent>
           <TextField
             autoFocus
@@ -31,6 +82,8 @@ function HomePage() {
             type="email"
             fullWidth
             variant="standard"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <TextField
             margin="dense"
@@ -39,21 +92,51 @@ function HomePage() {
             type="password"
             fullWidth
             variant="standard"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
+          {passwordError && <Alert severity="error">{passwordError}</Alert>}
+          {!isLogin && (
+            <>
+              <TextField
+                margin="dense"
+                id="firstname"
+                label="Jméno"
+                type="text"
+                fullWidth
+                variant="standard"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <TextField
+                margin="dense"
+                id="lastname"
+                label="Příjmení"
+                type="text"
+                fullWidth
+                variant="standard"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </>
+          )}
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Zrušit</Button>
-          <Button onClick={handleClose}>Přihlásit</Button>
+        <DialogActions style={{ justifyContent: 'space-between' }}>
+          <Box>
+            <Button onClick={handleClose}>Zrušit</Button>
+            <Button onClick={handleSubmit} color="primary">
+              {isLogin ? "Přihlásit" : "Registrovat"}
+            </Button>
+          </Box>
+          <Button
+            onClick={googleLogin}
+            variant="contained"
+            style={{ backgroundColor: '#4285F4', color: 'white' }}
+          >
+            {isLogin ? "Přihlásit se přes Google" : "Registrovat se přes Google"}
+          </Button>
         </DialogActions>
       </Dialog>
-      <Typography variant="h6" sx={{ mt: 4 }}>
-        Veřejné rozvrhy
-      </Typography>
-      {/* Zde by se zobrazil seznam veřejných rozvrhů */}
-      <Typography variant="h6" sx={{ mt: 4 }}>
-        Soukromé rozvrhy
-      </Typography>
-      {/* Po přihlášení zde budou zobrazeny soukromé rozvrhy */}
     </Box>
   );
 }
