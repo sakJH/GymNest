66ffe1ed512e805a107f830a344adc18867b1e39
sakacj <sakacja1@uhk.cz>
2024-03-24 14:52:35 +0100

    seq update

--- GymNest-IS/user-management-service/src/sequelize.js
+++ GymNest-IS/user-management-service/src/sequelize.js
@@ -4,7 +4,7 @@ require('dotenv').config(); // Načtení proměnných z .env souboru
 const sequelize = new Sequelize( // Nastavení DB login a heslo v .env
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_MYSQL_PASS,
     {
         host: process.env.DB_HOST,
         dialect: 'mysql'
