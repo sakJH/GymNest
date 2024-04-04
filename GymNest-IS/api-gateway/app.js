d3fd8d3d2dd73a7010a391d312d5fcbaea157c39
sakJH <79791046+sakJH@users.noreply.github.com>
2024-04-04 08:46:01 +0200

    snaha o opravu gateway

--- GymNest-IS/api-gateway/app.js
+++ GymNest-IS/api-gateway/app.js
@@ -7,7 +7,7 @@ const swaggerDefinition = require('./utils/swaggerDefinition');
 
 const app = express();
 const port = process.env.PORT || 8080;
-
+/*
 // Konfigurace CORS
 const corsOptions = {
     origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:8080 '], // Povolit requesty pouze z tohoto originu
@@ -17,6 +17,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions)); // Aplikace CORS middleware s nastavením
+*/
 app.use(express.json()); // Pro zpracování JSON requestů
 
 // Základní route
